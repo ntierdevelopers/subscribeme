0c0b9c721b120fbe6f6d0339e7dbceeedd94df13
pcdipalma <pcdipalma@gmail.com>
2017-10-01 22:31:17 -0400

    other files

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "d93571a37ee161630df238335f389bcc"
+  config.secret = "0ec5ff021fb72a37428bc854ba52b30b"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
